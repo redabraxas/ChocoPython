374e9241e4117665337f33fe078b4b714980fc06
redabraxas <redabraxas@naver.com>
2015-10-12 19:13:55 +0900

    외장함수

--- PythonApplication10/PythonApplication10/PythonApplication10.py
+++ PythonApplication10/PythonApplication10/PythonApplication10.py
@@ -40,23 +40,120 @@ import os, sys
 #print(list(os.walk("..")))
 
 
-#현재 디렉토리에 만든 후 txt파일 복사
-import shutil
-# 폴더가 잇는가?
+##현재 디렉토리에 만든 후 txt파일 복사
+#import shutil
+## 폴더가 잇는가?
 
-if os.path.exists('sample') == False:
-    os.mkdir("sample")
+#if os.path.exists('sample') == False:
+#    os.mkdir("sample")
 
 
-samplePath = os.path.abspath("sample")
+#samplePath = os.path.abspath("sample")
 
-#for (path, dir, files) in os.walk('.'):
-#    for dirname in dir:
-#        if dirname.find("sample") == -1:
-#            os.mkdir("sample")
+##for (path, dir, files) in os.walk('.'):
+##    for dirname in dir:
+##        if dirname.find("sample") == -1:
+##            os.mkdir("sample")
             
 
-for (path, dir, files) in os.walk('.'):
-    if path != "sample":
-        for filename in files:
-            if(filename.find('.txt') != -1):                shutil.copy(path+"/"+filename, samplePath+"/"+filename)os.path.expanduser('~\\python.exe')os.path.expandvars('$JAVA_HOME\\python.exe')
+#for (path, dir, files) in os.walk('.'):
+#    if path != "sample":
+#        for filename in files:
+#            if(filename.find('.txt') != -1):
+#                shutil.copy(path+"/"+filename, samplePath+"/"+filename)
+
+#os.path.expanduser('~\\python.exe')
+#os.path.expandvars('$JAVA_HOME\\python.exe')
+
+
+import glob
+print(glob.glob("*.txt"))
+
+
+import glob, os.path
+
+ndir = nfile = 0
+
+def traverse(dir, depth):
+    global ndir, nfile
+    for obj in glob.glob(dir + '/*'):
+        if depth == 0:
+            prefix = '|--'
+        else:
+            prefix = '|' + ' ' * depth + '|--'
+        if os.path.isdir(obj):
+            ndir += 1
+            print(prefix + os.path.basename(obj))
+            traverse(obj, depth+1)
+        elif os.path.isfile(obj) :
+            nfile +=1
+            print(prefix + os.path.basename(obj))
+        else:
+            print(prefix + 'unknown object:',obj)
+
+
+# 메인 패키지에서 동작될때
+if __name__ == '__main__':
+    traverse('..', 0)
+    print('\n',ndir,'directories,',nfile, 'files')
+
+
+
+
+# 기본모드 w+b -> 바이너리 바꿔줘야함
+#import tempfile
+#with tempfile.TemporaryFile('w+', delete=False) as fp:
+#    fp.write("heelo world")
+#    fp.seek(0)
+#    data = fp.read()
+#    print(data)
+#    print(fp.name)
+
+
+import time;
+
+t1 = time.time()
+
+a =time.strftime("%B %dth %A %I:%M", time.localtime())
+print(a)
+
+
+time1 = time.ctime(1234567)
+print(time1)
+print(time.strftime(time1))
+
+
+import calendar
+calendar.calendar(2000)
+calendar.prcal(2000)
+
+
+print(calendar.monthrange(2015,10))
+print(calendar.monthrange(1994,9))
+
+
+import random
+
+#중복없이 무작위로 뽑아줌
+
+#rr = random.sample(range(100),10)
+#print(rr)
+#rrr = random.shuffle(rr)
+#rrrr= random.choice(rrr)
+#print(rrrr)
+
+
+
+weighted_choices = [('Red', 3), ('Blue', 1), ('Yellow', 2), ('Green', 4)]
+population = [val for val, cnt in weighted_choices for i in range(cnt)]
+datalist =[]
+for val, cnt in weighted_choices:
+    for i in range(cnt):
+        datalist.append(val)
+
+print(population)
+
+import webbrowser
+url = 'http://google.com'
+webbrowser.open_new_tab(url)
+webbrowser.open_new(url)
