aa57664d4c23c46cd55dcf6952a32a3ee0347cfc
redabraxas <redabraxas@naver.com>
2015-10-28 14:12:08 +0900

    파싱

--- PythonApplication11/PythonApplication11/PythonApplication11.py
+++ PythonApplication11/PythonApplication11/PythonApplication11.py
@@ -102,13 +102,99 @@ Solaris'''
 # ^ 시작 . 줄바꿈을 제외한 문자 + 1개 이상
 p1 = re.compile('^.+', re.M)
 # re.M = 멀티라인 
-print(p1.findall(str))p2= re.compile('^.+', re.S)print(p2.search(str))## Group 이름 지정m = re.match(r"(?P<first>\w+) (?P<last>\w+)", "Malcolm Reynolds")print(m.group('first','last'))print(m.groups())print(m.groupdict()) # ditionary 출력## Group 디폴트값m = re.match(r"(\d+)\.?(\d+)?","24")print(m.groups())print(m.groups(0))p = re.compile(".+:") # : 앞에 하나 이상 아무것다 올 수 잇다m = p.search("http://google.com")print(m.group()) # http: 이 나옴.# : 앞에 해당하는 것만 결과 값으로 주겠다.p = re.compile(".+(?=:)")m = p.search("http://google.com")print(m.group())## 해당 정규식과 매치되어도 해당 문자열은 통과되지 않음p= re.compile('.*[.](?!bat$|exe$).*$')import os, globos.chdir('C:\\')a = glob.glob('.*[.](?!bat$|exe$).*$') # 현재 디렉토리 반환print(a)# abcdef 를 찾되 , def만 반환p = re.compile("(?<=abc)def")m = p.search("abcdefasdf")print(m.group())# - 뒤의 글자만 반환m = re.search('(?<=-)\w+', "spam-egg")print(m.group())# 매치되는 스트링의 사작과 긑 인덱스 반환 email = "tony@tiremove_thisger.net"m = re.search("remove_this", email)print(repr(m.start()) + "  " + repr(m.end()))result = email[:m.start()] + email[m.end():]print(result)# 그냥 매치 정보 출력 함수def displaymatch(match):    if match is None:        return None    return '<Match: %r, groups=%r>' % (match.group(), match.groups())# 다섯개의 문자열이 저렇게 끝나야한다.valid = re.compile(r"^[a2-9tjqk]{5}$")d1 = displaymatch(valid.match("akt5q"))print(d1)text = """Ross: McFluff: 834.345.1254: 155 Elm Street
+print(p1.findall(str))
+p2= re.compile('^.+', re.S)
+print(p2.search(str))
+
+## Group 이름 지정
+m = re.match(r"(?P<first>\w+) (?P<last>\w+)", "Malcolm Reynolds")
+print(m.group('first','last'))
+print(m.groups())
+print(m.groupdict()) # ditionary 출력
+
+## Group 디폴트값
+m = re.match(r"(\d+)\.?(\d+)?","24")
+print(m.groups())
+print(m.groups(0))
+
+
+p = re.compile(".+:") # : 앞에 하나 이상 아무것다 올 수 잇다
+m = p.search("http://google.com")
+print(m.group()) # http: 이 나옴.
+
+# : 앞에 해당하는 것만 결과 값으로 주겠다.
+p = re.compile(".+(?=:)")
+m = p.search("http://google.com")
+print(m.group())
+
+## 해당 정규식과 매치되어도 해당 문자열은 통과되지 않음
+p= re.compile('.*[.](?!bat$|exe$).*$')
+
+import os, glob
+os.chdir('C:\\')
+a = glob.glob('.*[.](?!bat$|exe$).*$') # 현재 디렉토리 반환
+print(a)
+
+# abcdef 를 찾되 , def만 반환
+p = re.compile("(?<=abc)def")
+m = p.search("abcdefasdf")
+print(m.group())
+
+# - 뒤의 글자만 반환
+m = re.search('(?<=-)\w+', "spam-egg")
+print(m.group())
+
+
+# 매치되는 스트링의 사작과 긑 인덱스 반환 
+email = "tony@tiremove_thisger.net"
+m = re.search("remove_this", email)
+print(repr(m.start()) + "  " + repr(m.end()))
+result = email[:m.start()] + email[m.end():]
+print(result)
+
+# 그냥 매치 정보 출력 함수
+def displaymatch(match):
+    if match is None:
+        return None
+    return '<Match: %r, groups=%r>' % (match.group(), match.groups())
+
+# 다섯개의 문자열이 저렇게 끝나야한다.
+valid = re.compile(r"^[a2-9tjqk]{5}$")
+d1 = displaymatch(valid.match("akt5q"))
+print(d1)
+
+text = """Ross: McFluff: 834.345.1254: 155 Elm Street
 Ronald: Heathmore: 892.345.3428 436: Finley Avenue
 Frank: Burger: 925.541.7625 662: South Dogwood Way
-Heather: Albrecht: 548.326.4584 919: Park Place"""# 엔터키를 기준으로 엔트리를 만들엇당.entries = re.split("\n", text)result = [re.split(":?", entry, 4) for entry in entries]print(result)## url 오픈import urllib.requestwith urllib.request.urlopen('http://www.python.org/') as f:    #print(f.read())    #print(f.read(300))    #print(f.read(300).decode("utf-8"))    # 타이틀 받아오기    content = f.read().decode("utf-8")
+Heather: Albrecht: 548.326.4584 919: Park Place"""
+
+# 엔터키를 기준으로 엔트리를 만들엇당.
+entries = re.split("\n", text)
+result = [re.split(":?", entry, 4) for entry in entries]
+print(result)
+
+
+
+## url 오픈
+import urllib.request
+
+with urllib.request.urlopen('http://www.python.org/') as f:
+    #print(f.read())
+    #print(f.read(300))
+    #print(f.read(300).decode("utf-8"))
+    # 타이틀 받아오기
+    content = f.read().decode("utf-8")
     result = re.search(r'<title>(.*)</title>', content)
     print(result.group(1))
-   ## url 파싱#from urllib.parse import urlparse#result = urlparse('http://search.naver.com/search.naver?where=nexearch&query=urllib.parse&sm=top_hty&fbm=1&ie=utf8')
+
+
+
+
+   
+
+## url 파싱
+#from urllib.parse import urlparse
+#result = urlparse('http://search.naver.com/search.naver?where=nexearch&query=urllib.parse&sm=top_hty&fbm=1&ie=utf8')
 #print(result)
 #print(result.scheme)
 #print(result.geturl())
---
+++ PythonApplication12/PythonApplication12/PythonApplication12.py
@@ -0,0 +1,89 @@
+﻿
+# bs4 로부터 BeatifulSoup 만 임포트.
+from bs4 import BeautifulSoup
+
+html_doc = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
+<p class="story">Once upon a time there were three little sisters; and their
+names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+<p class="story">...</p>
+"""
+
+# 파서 종류를 지정해줌. 디폴트가 html.parser
+# 내가 원하느 파서가 잇으면 따로 지정 가능
+soup = BeautifulSoup(html_doc, "html.parser")
+print(soup.prettify()) # 예쁘게 출력.
+print(soup.a.prettify())
+
+
+print(soup.html.head.title) # title에 해당되는 태그 내용을 보여줌
+print(soup.title)
+soup.title.string # title을 string으로 변경
+print(soup.p) # 같은 이름의 태그가 여러개면 제일 먼저 나오는 것 알려줌
+
+print(soup.p['class']) # p 태그 안의 속성 값 가져오기
+
+# p 태그를 가진것 전부 알려줌
+print("-----------------------------------")
+print(soup('a'))
+
+# 해당되는 태그값을 가진 것만 반환?
+print("-----------------------------------")
+s1 = soup('a',{'id':'link1'})
+print(s1)
+s2=soup('p',{'class':'story'})
+print(s2)
+
+
+# 계층구조속성 : 액세스한 것 기준으로 다시 액세스
+print("-----------------------------------")
+print(soup('a')[0].parent) # 부모자체
+soup('a')[0].parent.name # 부모의 이름
+soup('a')[0].contents # 자식
+soup('a')[0].NextSibling # 같은 위치 바로 앞
+soup('a')[0].previousSbling # 같은 위치 바로 뒤
+soup('a')[0].Next # 계층구조 무관 앞
+soup('a')[0].previous # 계층구조 무관 뒤
+
+
+# 처음 나오는 해당 태그 객체 반환
+soup.find('p')
+s1 =soup.find('a')['href'] # a 태그에서 href 해당하는 값을 가져옴
+print(s1)
+
+# 전체 문서에서 태그를 검색하여 리스트 형태로 반환
+soup.find_all(id='link3')soup.find_all('p')
+s2 = soup.find_all(class_='sister')print(s2)# 웹툰에서 가져오기 실습from urllib.parse import urljoinfrom urllib.request import urlopenimport webbrowserurl= "http://comic.naver.com/webtoon/list.nhn?titleId=449854&weekday=wed"data = urlopen(url)soup = BeautifulSoup(data, 'html.parser')cartoons = soup.find_all('td',{'class':'title'})print("-----------------------------------")for i in range(len(cartoons)):    title = cartoons[i].find('a').string    ref = cartoons[i].find('a')['href']    tempurl=urljoin(url,ref)    print(title,"  ",tempurl)    #webbrowser.open_new(tempurl)print("-----------------------------------")
+
+class crawler:
+    def crawl(self, pages, depth=2):
+        for i in range(depth):
+            newpage = set()
+            for page in pages:
+                try:
+                    c = urllib.request.urlopen(page)
+                except:
+                    print("Could not open %s" %page)
+                    continue
+                # encoding 옵션
+                soup = BeautifulSoup(c.read(), from_encoding="utf-8")
+                print('Found %s' %page)
+            links = soup('a')
+
+            for link in links:
+                if('href' in dict(link.attrs)):  # a 에 들어갈 수 있는 속성 중 href 찾음
+                    url = urllib.parse.urljoin(page, link['href'])
+                    if url.find("'")!=-1 : continue
+                    url = url.split("#")[0]
+                    if url[0:4]=='http': # http로 시작하는 url은 add
+                        newpage.add(url)
+            pages = newpage
+pagelist=['http://naver.com']
+crawler=crawler()
+crawler.crawl(pagelist)
